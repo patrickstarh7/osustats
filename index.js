94d43b26513272067138afd59baa6f526ee7e76b
Patrick <path.plh@gmail.com>
2022-01-24 18:48:08 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+//dependencies
+const express = require('express')
+const app = express();
+const PORT = 8000;
 
+app.use(express.json())
+
+//listens for port and calls back when api is ready
+app.listen(8000, () =>{
+    console.log(`it's alive on http://localhost:${PORT}/osustats`);
+})
+
+app.get('/osustats', (req, res) => {
+    console.log(req);
+    res.status(200).send({
+        username: 'patrickstarh7',
+    })
+});
+
+app.post('/osustats/:id', (req, res) => {
+    const {id} = req.params;
+    const {pfp} = req.body;
+
+    if(!pfp){
+        res.status(418).send({message: 'Default pfp!'})
+    }
+
+    res.send({
+        osu_player: `username with id of ${id} and pfp of ${pfp}`,
+    })
+
+});
